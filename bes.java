b2b16e879167d0419d5e575c33d396dee14a0a4b
ssabaldan569921-collab <s.sabaldan.569921@umindanao.edu.ph>
2026-04-13 17:25:13 +0800

    Add files via upload

---
+++ bes.java
@@ -0,0 +1,30 @@
+package bes;
+import javax.swing.*;
+import javax.swing.JOptionPane;
+public class bes {
+
+	public static void main(String[] args) {
+		int hotel[][] = new int [7][5];
+		int choice;
+		int availableRooms = 35;
+		int occupiedRooms = 0 ; 
+		
+		while (true) {
+			String input = JOptionPane.showInputDialog(
+					"Hotel Reservation System \n\n +"
+					+ "1. View Rooms\n +"
+					+ "2. Check In\n"
+					+ "3. Check Out\n"
+					+ "4. Exit\n\n"
+					+ "Enter Choice: ");
+				
+		}
+				
+	
+				
+		
+		
+		
+	}
+
+}
